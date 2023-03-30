f735358d429288782b4c370e72b29100eaf8e16e
Stas <112703038+silenthnowakeup@users.noreply.github.com>
2023-03-30 18:38:05 +0300

    Update main.c

--- decompressFile/main.c
+++ decompressFile/main.c
@@ -8,38 +8,24 @@ int main() {
         printf("Error file\n");
         exit(1);
     }
+    int numReplacements;
+    char** wordsA;
+    char** wordsB;
+    wordsA = (char**)malloc(16*sizeof(char*));
+    wordsB = (char**)malloc(16*sizeof(char*));
+    readWordsFromFile(compressFile,&wordsA,&wordsB,&numReplacements);
+    printf("%d\n",numReplacements);
 
-    char* word1 = (char*)calloc(16,sizeof(char));
-    char* word2 = (char*)calloc(4,sizeof(char));
-
-    fseek(compressFile, 0, SEEK_END);
-
-    long int fileSize = ftell(compressFile);
-
-    int pos = findPositions(compressFile);
-    fseek(compressFile, fileSize - pos, SEEK_SET);
-    if (fscanf(compressFile, "$%16s $%4s", word1, word2) == 2) {
-        printf("read word: %s and %s\n", word1, word2);
-    }
-    fseek(compressFile,0,SEEK_SET);
-    fclose(compressFile);
-
-
+    int length1 = printWordLengths(wordsA,numReplacements)+ printWordLengths(wordsB,numReplacements) + numReplacements*2;
+    printf("%d",length1);
+    fseek(compressFile,length1,SEEK_SET);
     FILE* decompressFile = fopen("D:/Lab2Sem2/cmake-build-debug/decompFile.txt", "w");
-    compressFile = fopen("D:/Lab2Sem2/cmake-build-debug/compFile.txt", "r");
-    printCompressedFile(compressFile,decompressFile,word1,word2);
-    free(word1);
-    free(word2);
-    if (decompressFile!=NULL) {
-        fclose(decompressFile);
-    }
-    else printf("error");
 
-    if(compressFile!=NULL) {
-        fclose(compressFile);
-    }
-    else printf("error");
 
+    printCompressedFile(compressFile, decompressFile,wordsA,wordsB,numReplacements);
+
+    fclose(compressFile);
+    fclose(decompressFile);
 
     return 0;
 }
