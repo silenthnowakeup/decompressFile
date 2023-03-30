a65b5a1ed0279ddd6977edc380f19ef914937f17
Stas <112703038+silenthnowakeup@users.noreply.github.com>
2023-03-30 18:37:14 +0300

    Update functions.c

--- decompressFile/functions.c
+++ decompressFile/functions.c
@@ -5,49 +5,72 @@
 #include "functions.h"
 
 
-int findPositions(FILE* compressFile)
-{
-    fseek(compressFile, 0, SEEK_END);
-    int count = 0;
-    int pos = 0;
-    while (count<2) {
-        fseek(compressFile, -1 - pos, SEEK_END);
-        int c = fgetc(compressFile);
-        if (c == '$'){
-            count++;
-        }
-        pos++;
-    }
-    printf("%d\n", pos);
-    return pos;
-}
+void readWordsFromFile(FILE* file, char*** wordsA, char*** wordsB, int* numReplacements) {
+    int size = 16;
+    char* word = (char*)calloc(size, sizeof(char));
+    int i = 0;
 
-void printCompressedFile(FILE* file, FILE* compressedFile, char* wordA, char* wordB) {
-    char* word = (char*)calloc(16,sizeof(char));
-    printf("\nWordA::%s, WordB::%s", wordA, wordB);
-    while (fscanf(file, "%16s", word) == 1) {
-
-        if (strstr(word, "$") != NULL) {
+    while (fscanf(file, "$%[^$\n]", word) == 1) {
+        char* secondWord = (char*)calloc(size, sizeof(char));
+        fscanf(file, "$%[^$\n]\n", secondWord);
+        (*wordsA)[i] = (char*)calloc(strlen(word) + 1, sizeof(char));
+        (*wordsB)[i] = (char*)calloc(strlen(secondWord) + 1, sizeof(char));
+        strcpy((*wordsA)[i], word);
+        strcpy((*wordsB)[i], secondWord);
+        free(secondWord);
+        i++;
+        if (i == *numReplacements) {
             break;
         }
+    }
 
-        int len = (int)strlen(word);
+    *numReplacements = i;
+    free(word);
+
+}
+
+void printCompressedFile(FILE* file,FILE* compressedFile, char** wordsA, char** wordsB, int numReplacements) {
+    int size = 16;
+    char* word = (char*)calloc(size,sizeof(char));
+
+    while (fscanf(file, "%16s", word) == 1) {
+        unsigned long len = strlen(word);
+
         if (ispunct(word[len - 1])) {
             char lastChar = word[len - 1];
-            word[len - 1] = '\0'; // жестко убирает знак препинания
-            if (strcmp(word, wordA) == 0) {
-                fprintf(compressedFile, "%s%c ", wordB, lastChar);
-            } else if (strcmp(word, wordB) == 0) {
-                fprintf(compressedFile, "%s%c ", wordA, lastChar);
-            } else {
+            word[len - 1] = '\0';
+            int found = 0;
+            for (int i = 0; i < numReplacements; i++) {
+                if (strcmp(word, wordsA[i]) == 0) {
+                    fprintf(compressedFile, "%s%c ", wordsB[i], lastChar);
+                    found = 1;
+                    break;
+                }
+                else if (strcmp(word, wordsB[i]) == 0) {
+                    fprintf(compressedFile, "%s%c ", wordsA[i], lastChar);
+                    found = 1;
+                    break;
+                }
+            }
+            if (!found) {
                 fprintf(compressedFile, "%s%c ", word, lastChar);
             }
-        } else {
-            if (strcmp(word, wordA) == 0) {
-                fprintf(compressedFile, "%s ", wordB);
-            } else if (strcmp(word, wordB) == 0) {
-                fprintf(compressedFile, "%s ", wordA);
-            } else {
+        }
+        else {
+            int found = 0;
+            for (int i = 0; i < numReplacements; i++) {
+                if (strcmp(word, wordsA[i]) == 0) {
+                    fprintf(compressedFile, "%s ", wordsB[i]);
+                    found = 1;
+                    break;
+                }
+                else if (strcmp(word, wordsB[i]) == 0) {
+                    fprintf(compressedFile, "%s ", wordsA[i]);
+                    found = 1;
+                    break;
+                }
+            }
+            if (!found) {
                 fprintf(compressedFile, "%s ", word);
             }
         }
@@ -56,3 +79,12 @@ void printCompressedFile(FILE* file, FILE* compressedFile, char* wordA, char* wo
     fseek(compressedFile,0,SEEK_END);
     ftruncate(fileno(compressedFile), ftell(compressedFile) - 1);
 }
+
+int printWordLengths(char** words, int n) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += strlen(words[i]);
+        }
+        return sum;
+    }
+
